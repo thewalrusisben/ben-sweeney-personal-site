da81979d34b6d807eec5acc840fa1d26000961c2
thewalrusisben <bensweeney23@gmail.com>
2017-06-04 16:02:31 -0400

    set up a game of ping pong

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+var express = require('express')
+var path = require('path')
+var axios = require('axios')
+var app = express()
 
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-app.listen(process.env.PORT || 9000);
+setInterval(() => {
+  console.log('ping...')
+  axios.get(`http://localhost:8081/pingpong`)
+  .then((response) => {
+    console.log(`${response.data.message}...`)
+  })
+}, 10000)
+
+app.listen(process.env.PORT || 9000)
