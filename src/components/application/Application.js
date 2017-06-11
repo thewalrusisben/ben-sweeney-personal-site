a0271fc76f5031c2aba260c51da87b469e0a9a4b
thewalrusisben <bensweeney23@gmail.com>
2017-06-11 18:21:25 -0400

    add github repo link

--- src/components/application/Application.js
+++ src/components/application/Application.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import './Application.css'
 
+
+class Subtitle extends Component {
+  render () {
+    if (this.props.github) {
+      return (
+        <div>
+          <a href={this.props.github} target="_blank">
+            <img src={"./github.png"} className="subtitle-icon" />
+            <span className="appSubtitle">{this.props.subtitle}</span>
+          </a>
+        </div>
+      )
+    } else {
+      return (
+        <div><span className="appSubtitle">{this.props.subtitle}</span></div>
+      )
+    }
+  }
+}
+
 class Application extends Component {
   render () {
     return (
       <li className="appListItem">
         <div className="appHeader">
           <h2 className="appTitle"><a href="" target="_blank">{this.props.title}</a></h2>
-          <span className="appSubtitle">{this.props.subtitle}</span>
+          <Subtitle github={this.props.github} subtitle={this.props.subtitle} />
         </div>
-        <img src={this.props.image_url} alt={`${this.props.title} Application Screen Shot`} />
+        { this.props.image_url ? <img id="screenshot" src={this.props.image_url} alt={`${this.props.title} Application Screen Shot`} /> : <div />}
         <div className="appDescription">{this.props.description}</div>
       </li>
     )
